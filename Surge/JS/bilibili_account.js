3d2bc295a342bd56f04df79a6085c605ae211e00
travisoa <travis.zhutao@gmail.com>
2020-01-26 22:04:36 +0800

    更新哔哩哔哩

--- Surge/JS/bilibili_account.js
+++ Surge/JS/bilibili_account.js
@@ -1,16 +1,29 @@
-/*
-surge 4:
-http-response https://app.bilibili.com/x/v2/account/mine\?access_key requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/travisoa/Script/master/Surge/JS/bilibili_account.js
-*/
-
 let body = $response.body
-body=JSON.parse(body)
-body['data']['sections'].splice(0,1)
-body['data']['sections'][0]['items'].splice(3,1)
-body['data']['sections'][0]['items'].splice(4,3)
-body['data']['sections'].splice(1,1)
-body=JSON.stringify(body)
-$done({body})
 
+body = JSON.parse(body)
+body['data']['sections'] = [
+    {
+        "title": "个人中心",
+        "items": [
+            {             
+                "title": "历史记录",
+                "uri": "bilibili://user_center/history"
+            },
+            {   
+                "title": "我的收藏",              
+                "uri": "bilibili://user_center/favourite"
+            },
+            {
+                "title": "稍后再看",
+                "uri": "bilibili://user_center/watch_later"
+            },
+            {
+                "title": "离线缓存",
+                "uri": "bilibili://user_center/download"
+            },
+        ]
+    }
+]
 
-//by:onewayticket255
+body = JSON.stringify(body)
+$done({ body })
