bb5dd0dcfad84a8783bfa1f72016be81759ea933
travisoa <travis.zhutao@gmail.com>
2020-02-03 17:53:58 +0800

    a

--- QuantumultX/File/documents.js
+++ QuantumultX/File/documents.js
@@ -1,18 +1,3 @@
-var obj = JSON.parse($response.body);
-obj['originalTransactionId'] = "20000625420102";
-obj['subscriptionState'] = "trial";
-obj['isInGracePeriod'] = false;
-obj['subscriptionExpirationDate'] = "17:48 25/11/2099";
-obj['isDocuments6User'] = true;
-obj['isEligibleForIntroPeriod'] = false;
-obj['subscriptionAutoRenewStatus'] = "autoRenewOff";
-obj['subscriptionReceiptId'] = "1530908572000";
-
-
-$done({body: JSON.stringify(obj)});
-
-
-/*
 var obj= {
   "originalTransactionId" : "20000625420102",
   "subscriptionState" : "trial",
@@ -25,6 +10,5 @@ var obj= {
 };
 
 $done({body: JSON.stringify(obj)});
-*/
 
 // Descriptions
