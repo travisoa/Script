32438dc281ebb206b91cc734e59112757509c902
travisoa <travis.zhutao@gmail.com>
2020-02-10 16:32:34 +0800

    a

--- QuantumultX/File/ireaderdown.js
+++ QuantumultX/File/ireaderdown.js
@@ -2,6 +2,6 @@
 var obj = JSON.parse($response.body); 
 obj['body']['is_recharged'] = true;
 obj['body']['price_per_unit'] = 0;
-obj['body']['fee_info']['is_vip_used'] = true;
+//obj['body']['fee_info']['is_vip_used'] = true;
 obj['body']['fee_info']['price'] = 0;
 $done({body: JSON.stringify(obj)});
