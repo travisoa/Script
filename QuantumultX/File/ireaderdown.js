7454315bcdce244fcdd68b56413bea546b594ca2
travisoa <travis.zhutao@gmail.com>
2020-02-17 17:30:32 +0800

    hh

--- QuantumultX/File/ireaderdown.js
+++ QuantumultX/File/ireaderdown.js
@@ -4,4 +4,7 @@ obj['body']['is_recharged'] = true;
 obj['body']['price_per_unit'] = 0;
 //obj['body']['fee_info']['is_vip_used'] = true;
 obj['body']['fee_info']['price'] = 0;
+obj['body']['fee_info']['original_price'] = 0;
+
+
 $done({body: JSON.stringify(obj)});
