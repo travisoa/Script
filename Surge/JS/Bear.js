09925d79ccc6243209a12362e8abba8938c812f9
travisoa <travis.zhutao@gmail.com>
2020-03-03 23:11:10 +0800

    up

--- Surge/JS/Bear.js
+++ Surge/JS/Bear.js
@@ -1,11 +1,11 @@
 /*
-Bear笔记
+Bear笔记 by hello1983
 
 [MITM]
 hostname = buy.itunes.apple.com
 
 [Script]
-http-response ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Bear.js,script-update-interval=0
+http-response ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/travisoa/Script/master/Surge/JS/Bear.js,script-update-interval=0
 */
 
 var obj = JSON.parse($response.body);
