ae37343e32fce34a943759fe217d3b7fe3c9fa57
travisoa <travis.zhutao@gmail.com>
2020-03-10 00:43:51 +0800

    up

--- QuantumultX/File/ireaderdown.js
+++ QuantumultX/File/ireaderdown.js
@@ -8,3 +8,10 @@ obj['body']['fee_info']['original_price'] = 0;
 
 
 $done({body: JSON.stringify(obj)});
+
+/*
+美区掌阅
+^https:\/\/api\.ireader\.mobi\/download\/fee_tpl\/.* url script-response-body NobyDa/Surge/JS/ireaderdown.js
+
+Mimt=api.ireader.mobi
+*/
