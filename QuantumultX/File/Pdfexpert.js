4a471b9bd0d73b2de01845e3c296f4c7a4ad54b4
travisoa <travis.zhutao@gmail.com>
2021-12-11 00:31:44 +0800

    Pdfexpert.js

--- QuantumultX/File/Pdfexpert.js
+++ QuantumultX/File/Pdfexpert.js
@@ -1,14 +1,19 @@
-var obj= {
-  "originalTransactionId": "20000618444996",
-  "subscriptionState": "trial",
-  "isInGracePeriod": false,
-  "subscriptionExpirationDate": "13:15 03/11/2099",
-  "subscriptionAutoRenewStatus": "autoRenewOn",
-  "isEligibleForIntroPeriod": false,
-  "isPDFExpert6User": false,
-  "subscriptionReceiptId": "1572178404000"
-};
 
-$done({body: JSON.stringify(obj)});
+re('\\w{1,2}\/\\w{1,2}\/\\w{4}@"subscriptionState":"\\w+"@"is(.+)User":false','25/11/2099@"subscriptionState":"active"@"is$1User":true');
 
-//
+function re() {
+var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done({body});
+} 
